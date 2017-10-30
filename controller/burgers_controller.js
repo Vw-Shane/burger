5cc569e1d954dab31cc8b591f0fc99b2f8b5fee5
Shane Orend <wagen00@gmail.com>
2017-10-30 15:20:48 -0400

    Burger Test

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -3,14 +3,11 @@ var burger = require("../models/burger.js");
 var router = express.Router();//  this is what routes api calls
 
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burger: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+ 
+    
+    res.render("index");
   });
-});
+
 
 
 module.exports = router; 
