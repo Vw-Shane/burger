58246a176537cb976c665b6ed94c5e3e032ae7db
Shane Orend <wagen00@gmail.com>
2017-10-30 12:36:26 -0400

    nonworking Skeleton code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jerry", 90);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Elaine");
+INSERT INTO burgers (burger_name, devoured, ) VALUES ("Kramer", 20);
+
